0c341552bdcbd9c007a3d9e217fb8b11e386d712
raflirasyidin <raflirasyidin20@gmail.com>
2024-01-22 09:55:50 +0700

    prepare favorite repository for local data source

---
+++ lib/data/repository/FavoriteRepository.dart
@@ -0,0 +1,39 @@
+import 'package:dish_dash/data/local/entities/FavoriteEntity.dart';
+
+import '../local/dao/FavoriteDao.dart';
+
+abstract class FavoriteRepository {
+  Future<List<Favorite>> getFavorites();
+  Future<Favorite?> getFavoriteById(String id);
+  Future<void> insertFavorite(Favorite favorite);
+  Future<void> deleteFavorite(Favorite favorite);
+}
+
+class FavoriteRepositoryImpl implements FavoriteRepository {
+  final FavoriteDao _dao;
+
+  FavoriteRepositoryImpl(this._dao);
+
+  @override
+  Future<Favorite?> getFavoriteById(String id) async {
+    final data = await _dao.getFavoriteById(id);
+    return data;
+  }
+
+  @override
+  Future<List<Favorite>> getFavorites() async {
+    final data = await _dao.getFavorites();
+    return data;
+  }
+
+  @override
+  Future<void> insertFavorite(Favorite favorite) async {
+    await _dao.insertFavorite(favorite);
+  }
+
+  @override
+  Future<void> deleteFavorite(Favorite favorite) async {
+    await _dao.deleteFavorite(favorite);
+  }
+
+}
--- lib/di/Locator.dart
+++ lib/di/Locator.dart
@@ -1,6 +1,8 @@
 import 'package:dio/dio.dart';
+import 'package:dish_dash/data/local/dao/FavoriteDao.dart';
 import 'package:dish_dash/data/local/db/dish_dash_database.dart';
 import 'package:dish_dash/data/remote/api/ApiRestaurant.dart';
+import 'package:dish_dash/data/repository/FavoriteRepository.dart';
 import 'package:dish_dash/data/repository/RestaurantRepository.dart';
 import 'package:get_it/get_it.dart';
 
@@ -10,7 +12,9 @@ final locator = GetIt.instance;
 
 Future<void> initDependencies() async {
   final db = await $FloorDishDashDatabase.databaseBuilder(DishDashDatabase.dbName).build();
-  locator.registerSingleton<DishDashDatabase>(db);
+  locator.registerSingleton(db);
+  locator.registerSingleton(db.favoriteDao);
+  locator.registerSingleton(FavoriteRepositoryImpl(locator<FavoriteDao>()));
 
   final dio = Dio(
       BaseOptions(
@@ -20,9 +24,7 @@ Future<void> initDependencies() async {
         sendTimeout: const Duration(seconds: 30),
       )
   );
-  locator.registerSingleton<Dio>(dio);
-
-  locator.registerSingleton<ApiRestaurant>(ApiRestaurant(locator<Dio>()));
-
+  locator.registerSingleton(dio);
+  locator.registerSingleton(ApiRestaurant(locator<Dio>()));
   locator.registerSingleton(RestaurantRepositoryImpl(locator<ApiRestaurant>()));
 }
